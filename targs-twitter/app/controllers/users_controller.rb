757b256d13a8a15941bcdd0310265302a6db015e
Patrick S. Betti & Alex Kass & Kelvin Gann & Ian & Karate Kid <pair+PatrickShelby.kassanova.klvngnn.raidersofthenight.sbsample@devbootcamp.com>
2015-06-12 15:50:35 -0700

    finished login

--- targs-twitter/app/controllers/users_controller.rb
+++ targs-twitter/app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
-get'/' do
-  redirect to '/users/register'
+get '/' do
+  redirect '/users/register'
 end
 
 ##INDEX
@@ -14,8 +14,13 @@ end
 
 ##CREATE
 post '/users' do
-  User.create(params[:user])
-  redirect '/users'
+  new_user = User.new(
+    :username => params[:username],
+    :email => params[:email]
+    )
+  new_user.password = params[:password]
+  new_user.save
+  redirect "/users/#{new_user.id}"
 end
 
 ##SHOW
@@ -39,47 +44,3 @@ delete '/users/:id' do
   User.delete(params[:id])
   redirect '/users'
 end
-# get'/' do
-#   erb :registration_form
-# end
-
-# ##INDEX
-# get '/users' do
-#   erb :users
-# end
-
-# ##NEW
-# get '/users/register' do
-#   erb :registration_form
-# end
-
-# ##CREATE
-# post '/users' do
-#   # p "*" *90
-#   # p params[:user]
-#   # p "*" *90
-#   User.create(params[:user])
-#   redirect '/users'
-# end
-
-# ##SHOW
-# get '/users/:id' do
-#   erb :profile
-# end
-
-# ##EDIT
-# get '/users/:id/edit' do
-#   erb :edit_profile
-# end
-
-# ##UPDATE
-# put '/users/:id' do
-#   User.update(params[:user])
-#   redirect '/users/:id'
-# end
-# #DELETE
-# delete '/users/:id' do
-#   #????
-#   User.delete(params[:id])
-#   redirect '/users'
-# end
